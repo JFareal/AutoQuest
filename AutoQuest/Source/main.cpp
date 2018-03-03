5366ab41f6fbf1f7b21158bdc7b9334692208dd5
THE-BEAST\TheBeast <farealdeal@gmail.com>
2018-03-02 23:14:07 -0600

    Created Function for Inputting Attributes.

--- AutoQuest/Source/main.cpp
+++ AutoQuest/Source/main.cpp
@@ -5,7 +5,41 @@ Main.cpp
 The Entry point of the application.
 *************************************************************************************************
 */
+#include <iostream>
+using namespace std;
 
+/*
+
+AutoQuest is a Programmatic RPG.
+
+
+User Enters Character information.
+
+Character Fights programmatic monsters
+
+Character Levels up.
+
+Character Gets Stronger
+
+Character Fights Harder Monsters.
+
+Rinse Repeat.
+
+*/
+
+
+typedef char CharacterAttribute;
+
+CharacterAttribute AskPlayerForAttribute( const char * attributeName ) {
+	// Reads out "Enter Your Strength: "
+	cout << "Enter your " << attributeName << ": " << endl;
+
+	char inputString[ 256 ];
+	cin >> inputString;
+
+	CharacterAttribute attribute = atoi( inputString );
+	return attribute;
+}
 
 /*
 ===============================================
@@ -14,12 +48,14 @@ main
 */
 int main() {
 
+	char name[ 256 ];
 
-	// Hello
+	cout << "Enter Your Name!" << endl;
+	cin >> name;
 
-
-	// OMG
-
+	cout << " Hello " << name << " Welcome to AutoQuest!" << endl;
+	
+	CharacterAttribute strength = AskPlayerForAttribute( "strength" );
 
 
 	return 0;
